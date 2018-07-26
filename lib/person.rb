565356e1a277d1e5896044ae430f95f1c8b9c00c
brodiethiele <brodie.thiele@gmail.com>
2018-07-26 19:46:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,15 @@
 # person.rb
 
-class Person
+class Person(name)
+  def initialize(name)
+    @name=name
 end 
+
+def name
+  @name
+end
+end
+
 
 person1 = Person.new("adele_goldberg")
 person2 = Person.new("alan_kay")
